17cab57c1f1bae0ff3b07f3e17ec8b802743451b
Alexandr Groshev <haddystuff@gmail.com>
2019-11-05 17:48:37 +0300

    I devide business logic and UI to different files(tasker, lazylab). So now lazylab contain UI. It calls functions from tasker file to do things.

--- lazylab.py
+++ lazylab.py
@@ -1,111 +1,11 @@
 import sys
-import yaml
-import libvirt
-from base_manage_vm import BaseManageVM
-from juniper_vmx_14_manage_all import JuniperVMX14ManageAll
-from cisco_iosxr_15_manage_all import CiscoIOSXR15ManageAll
-from config_parser import *
-from zipfile import ZipFile
-import os
-import ftplib
-
+from tasker import deploy_lab
+from tasker import delete_lab
 
 """
-This script is actually containing UI and business logic in one place so we need to divide it as soon as possible
+This is pretty simple UI
+Its calling tasker functions
 """
-
-
-def download_template_image(distribution):
-    with ftplib.FTP(IMAGES_FTP, 'anonymous', 'anonymous@domain.com') as ftp:
-        ftp.cwd('/pub')
-        with open(VOLUME_POOL_DIRECTORY + distribution + '_template.qcow2', 'wb') as f:
-            ftp.retrbinary('RETR ' + distribution + '_template.qcow2', f.write)
-    return(0)
-
-def check_if_template_image_exist(distribution):
-    if os.path.isfile(VOLUME_POOL_DIRECTORY + distribution + '_template.qcow2'):
-        return(0)
-    else:
-        print('No' + distribution + 'image\nDownloading...')
-        download_template_image(distribution)
-
-
-def yaml_validate(conf_yaml):
-    """
-    This function check if yaml file has right structure
-    """
-    #Getting list of vms
-    list_of_vms = conf_yaml["vms"]
-    
-    #Check if vms is actualy existing
-    if list_of_vms == None:
-        print("No \"vms\" block in config file")
-        exit(1)
-    #Check if lab name existing
-    if conf_yaml["lab_name"] == None:
-        print("No \"lab_name\" block in config file")
-        exit(1)
-    
-    #Check vm one by one.
-    for vm in list_of_vms:
-        #Check if name of vm is actially exist
-        if vm['name'] == None:
-            print("No \"VM Name\" block in config file")
-            exit(1)
-        
-        #Check if vm has right os and version(distibution in this context)
-        distribution = vm['os'] + '_' + str(vm['version'])
-        
-        if not(distribution in POSSIBLE_OS_LIST):
-            print('Yaml file has bad syntax: wrong os name')
-            exit(1)
-        
-        #Check if image of os exist on local disk
-        check_if_template_image_exist(distribution)
-    return(0)
-
-
-def create_device_dict_with_archive(config_archive_location):
-    """
-    This function creating dict of manage objects from zip archive. It actially looks like this:
-    { Testlab_router1: CiscoIOSXR15ManageAll_object
-      Testlab_router2: JuniperVMX14ManageAll_object
-    }
-    """
-    #Opening config file in zip archive, parsing with yaml and sending to conf_yaml valiable
-    with ZipFile(config_archive_location, 'r') as lazy_archive:
-        conf_yaml = yaml.load(lazy_archive.read(CONFIG_FILE_NAME), Loader=yaml.FullLoader)
-    
-    #Validating syntax and more of conf_yaml
-    yaml_validate(conf_yaml)
-    
-    #Setting some valiables
-    lab_name = conf_yaml["lab_name"]
-    cur_port = TELNET_STARTING_PORT
-    list_of_vms = conf_yaml["vms"]
-    devices = {}
-    
-    #Creating vm dictionary called "devices" one by one 
-    for vm in list_of_vms:
-        vm_config_file = vm['name'] + '.conf'
-        
-        #Getting config of device from zip archive
-        try:
-            with ZipFile(config_archive_location, 'r') as lazy_archive:
-                vm_config = lazy_archive.read(vm_config_file).decode("utf-8")
-        except Exception as err:
-            vm_config = None
-        
-        cur_port += 1
-        distribution = (vm['os'] + '_' + str(vm['version']))
-        
-        #Creating objects base on its OS
-        if (distribution) == 'juniper_vmx_14':
-            devices[lab_name + '_' + vm['name']] = JuniperVMX14ManageAll(lab_name = lab_name, vm = vm, port = cur_port, vm_config = vm_config)
-        elif (distribution) == 'cisco_iosxr_15':
-            devices[lab_name + '_' + vm['name']] = CiscoIOSXR15ManageAll(lab_name = lab_name, vm = vm, port = cur_port, vm_config = vm_config)
-    return(devices)  
-
 
 if __name__ == "__main__":
     
@@ -123,24 +23,10 @@ if __name__ == "__main__":
        
     #Parsing arguments and working with manage objects
     if sys.argv[1] == 'deploy':
-        # Create dictionary of managment objects using function
-        print('Deploying lab')
-        devices = create_device_dict_with_archive(config_archive_location)
-        #Deploying step by step. Methods of managment object is actually self explanitory.
-        for device in devices:
-            devices[device].create_net()
-            devices[device].clone_volume()
-            devices[device].create_vm()
-            devices[device].waiting()
-            devices[device].configure_vm()
+        deploy_lab(config_archive_location)
             
     elif sys.argv[1] == 'delete':
-        # Deleting vms obviosly
-        print('Deleting lab')
-        devices = create_device_dict_with_archive(config_archive_location)
-        for device in devices:
-            devices[device].destroy_vm()
-            devices[device].delete_volume()
+        delete_lab(config_archive_location)
     #Working on this
     #    elif sys.argv[1] == 'save':
     #        # Save configs
