e627b4490e2c439b23b4356e6c64b3b1d48f822a
Alexandr Groshev <haddystuff@gmail.com>
2020-01-20 04:34:40 +0300

    add more unit tests

--- test/base/base_all.py
+++ test/base/base_all.py
@@ -6,36 +6,47 @@ from xml.etree import ElementTree
 import libvirt
 import os.path
 
+TEST_VM_PARAMETERS = {'juniper_vmx_14': {'name': 'Tested_VM', 
+                                        'os': 'juniper_vmx',
+                                        'version': 14, 
+                                        'interfaces': {'ge-0/0/0': 'Unknown_net'}},
+                      'juniper_vmxvcp_17': {'name': 'Tested_VM', 
+                                           'os': 'juniper_vmxvcp',
+                                           'version': 18, 
+                                           'interfaces': {'ge-0/0/0': 'Unknown_net'}}}
+
+
 class BaseAll(unittest.TestCase):
     def setUp(self):
         """
         Creating class and object to work with
         """
+        self.devices = {}
         class_parents_tuple = (BaseManageVM,)
         lab_name = 'Test_Lab'
-        vm_parameters = {'name': 'Tested_VM', 
-                         'os': 'juniper_vmx',
-                         'version': 14, 
-                         'interfaces': {'ge-0/0/0': 'Unknown_net'}}
-        DeviceClass = type('JuniperVMX14ManageAll', class_parents_tuple, {})
-        self.device = DeviceClass(lab_name=lab_name, vm_parameters=vm_parameters)
-        self.clone_to_path = None
+
+        for distribution, vm_parameters in TEST_VM_PARAMETERS.items():
+            class_name = vm_parameters.get('os') + str(vm_parameters.get('version')) + 'ManageAll'
+            DeviceClass = type(class_name, class_parents_tuple, {})
+            self.devices[distribution] = DeviceClass(lab_name=lab_name, vm_parameters=vm_parameters)
         
     def test_clone_delte_volume(self):
-        self.device.clone_volume()
-        clone_from_path = f'{PATH_TO_MODULE}/images/juniper_vmx_14_template.qcow2'
         
-        template_volume_list = TEMPLATE_IMAGE_LIST.get(self.device.distribution)
-        for template_volume_name in template_volume_list:
-            with libvirt.open('qemu:///system') as conn: 
-                volume_pool = conn.storagePoolLookupByName(VOLUME_POOL_NAME)
-                volume_pool_xml_root = ElementTree.fromstring(volume_pool.XMLDesc())
-                target = next(volume_pool_xml_root.iter('target'))
-                volume_pool_path = next(target.iter('path')).text
-                clone_to_path = volume_pool_path + '/' + self.device.vm_name + template_volume_name
-            with self.subTest(action='clone'):
-                self.assertTrue(os.path.exists(clone_to_path))
+        for distribution in self.devices:
+            device = self.devices[distribution]
+            device.clone_volume()
             
-        self.device.delete_volume()
-        with self.subTest(action='delete'):
-            self.assertTrue(not (os.path.exists(clone_to_path)))
+            template_volume_list = TEMPLATE_IMAGE_LIST.get(device.distribution)
+            for template_volume_name in template_volume_list:
+                with libvirt.open('qemu:///system') as conn: 
+                    volume_pool = conn.storagePoolLookupByName(VOLUME_POOL_NAME)
+                    volume_pool_xml_root = ElementTree.fromstring(volume_pool.XMLDesc())
+                    target = next(volume_pool_xml_root.iter('target'))
+                    volume_pool_path = next(target.iter('path')).text
+                    clone_to_path = volume_pool_path + '/' + device.vm_name + template_volume_name
+                with self.subTest(action='clone', distribution=distribution):
+                    self.assertTrue(os.path.exists(clone_to_path))
+                
+            device.delete_volume()
+            with self.subTest(action='delete', distribution=distribution):
+                self.assertTrue(not (os.path.exists(clone_to_path)))
